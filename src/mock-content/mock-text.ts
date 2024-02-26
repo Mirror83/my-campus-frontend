bf8285658cca2a3f10afe3f8fdbe71e07b3129e5
Mirror83 <glenochieng045@gmail.com>
2024-02-26 22:36:22 +0300

    Added mock-text.ts for all mock textual data

---
+++ src/mock-content/mock-text.ts
@@ -0,0 +1,33 @@
+export const blogPlaceholder: string = `
+<h2>
+  Hi there,
+</h2>
+<p>
+  this space is where you'll write <em>your</em> story. You can have:
+</p>
+<ul>
+  <li>
+    a bullet list with one …
+  </li>
+  <li>
+    … or two list items (or more).
+  </li>
+</ul>
+<p>
+  Or, a code block:
+</p>
+<pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+<p>
+  and more, to help you convey your message. Give it a try and click a little bit around. And when you're 
+  ready, erase this and start from a fresh canvas.
+</p>
+<blockquote>
+“The secret of getting ahead is getting started.
+ The secret of getting started is breaking your complex overwhelming tasks into small manageable tasks,
+ and then starting on the first one.”
+  <br />
+  — Mark Twain
+</blockquote>
+`
