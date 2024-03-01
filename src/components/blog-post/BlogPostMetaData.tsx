c582842e6d42b80b5a15148272ad0a0a74c71e46
Mirror83 <glenochieng045@gmail.com>
2024-03-01 21:53:55 +0300

    Increased vertical padding of the two sections

--- src/components/blog-post/BlogPostMetaData.tsx
+++ src/components/blog-post/BlogPostMetaData.tsx
@@ -16,12 +16,12 @@ export function BlogPostMetaData({
 }: BlogPostMetaDataProps) {
   return (
     <>
-      <div className="flex gap-2 py-1 items-center text-sm">
+      <div className="flex gap-4 py-4 items-center text-sm">
         <AuthorAvatar src="https://github.com/shadcn.png" authorName="shadcn" />
         <strong>{authorName}</strong>
         <em>{dateCreated}</em>
       </div>
-      <div className="flex gap-1 py-1 items-center">
+      <div className="flex gap-4 py-1 items-center">
         <div className="flex gap-1 items-center">
           <ChatBubbleIcon />
           <em>{totalComments}</em>
