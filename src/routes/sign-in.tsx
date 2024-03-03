2c295633b9891e2165d13bfdd81d913538ccbd90
Mirror83 <glenochieng045@gmail.com>
2024-03-03 19:16:53 +0300

    Added the visual elements of the sign-in and sign-up pages

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,7 +1,127 @@
+import GraduationCapIcon from "@/components/home/GraduationCapIcon"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useNavigate } from "react-router"
+import { Link } from "react-router-dom"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters long",
+  }),
+  password: z.string().min(8, {
+    message: "Username must be at least 8 characters long",
+  }),
+})
+
+// Styles that can be placed on the black box div to insert an image.
+// Too bad the image I chose does not blend with the colours currently present
+// Credit: Photo by <a href="https://unsplash.com/@von_co?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ivana Cajina</a> on <a href="https://unsplash.com/photos/smiling-woman-wearing-gray-hoodie-dnL6ZIpht2s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+// const possibleBgImage = {
+//   backgroundImage: "url(images/ivana-cajina-dnL6ZIpht2s-unsplash.jpg)",
+//   backgroundRepeat: "no-repeat",
+//   backgroundSize: "cover"
+// }
+
 export function SignIn() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+
+  const navigate = useNavigate()
+
+  function onSubmit(formValues: z.infer<typeof formSchema>) {
+    console.log(formValues)
+    // Sign-in logic will be carried out by React Router action for this page
+    // Will have to use submit instead of navigate
+    navigate("/")
+  }
+
   return (
-    <>
-      <h1>Sign-in</h1>
-    </>
+    <div className={"grid h-[100vh] grid-cols-[2fr_3fr]"}>
+      <div className="bg-black"></div>
+      <div className="flex flex-col items-center justify-center">
+        <Card className="w-[400px]">
+          <CardHeader>
+            <CardTitle>
+              <div className="flex gap-1 items-center text-2xl">
+                <GraduationCapIcon />
+                <h1>My Campus</h1>
+              </div>
+            </CardTitle>
+            <CardDescription className="text-xl">Welcome back</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-2 mb-4">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input placeholder="mycampus" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="nic#e*a45nd@se   c?UR )"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button>Sign in</Button>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter>
+            <p className="text-light text-sm text-gray-400">
+              Don't have an account?{" "}
+              <Link to="/sign-up" className="underline text-gray-600">
+                Sign up
+              </Link>{" "}
+              for free.
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
   )
 }
--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,7 +1,189 @@
+import GraduationCapIcon from "@/components/home/GraduationCapIcon"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
+import { z } from "zod"
+
+// The naming convention used by the server is snake case
+// I know, it brings a lot of confusion 😂.
+const formSchema = z.object({
+  email: z
+    .string()
+    .regex(new RegExp("/^[A-Za-z0-9._%-]+@students.jkuat.ac.ke$/"), {
+      message: "The email must be a JKUAT student email.",
+    }),
+  // Find a way to make the names into title case (or let the server do it)
+  first_name: z.string().min(2, {
+    message: "First name should be at least 2 characters long",
+  }),
+  last_name: z.string().min(2, {
+    message: "Last name should be at least 2 characters long",
+  }),
+  username: z.string().min(2, {
+    message: "A username must be at least 2 characters long",
+  }),
+  password: z.string().min(8, {
+    message: "Your password must be at least 8 characters long",
+  }),
+
+  re_password: z.string().min(8, {
+    message: "Your password must be at least 8 characters long",
+  }),
+})
+
+// TODO: Store the layout that this shares with the sign-in page
+//       and use that as a root layout for both to reduce duplication
 export function SignUp() {
+  const form = useForm<z.infer<typeof formSchema>>()
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+    // Same as in SignIn, handle submission using the action for this route
+    // Will probably move to the next step
+  }
+
   return (
-    <>
-      <h1>Sign up</h1>
-    </>
+    <div className={"grid h-[100vh] grid-cols-[2fr_3fr]"}>
+      <div className="bg-black"></div>
+      <div className="flex flex-col items-center justify-center">
+        <Card className="w-[500px] h-[100vh] overflow-y-auto my-4">
+          <CardHeader>
+            <CardTitle>
+              <div className="flex gap-1 items-center text-2xl">
+                <GraduationCapIcon />
+                <h1>My Campus</h1>
+              </div>
+            </CardTitle>
+            <CardDescription className="text-xl">
+              Create an account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-2 mb-4">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="kimani.mwendwa@students.jkuat.ac.ke"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="first_name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>First name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Kimani" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="last_name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Last name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="kimani.mwendwa@students.jkuat.ac.ke"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input placeholder="mycampus" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="nic#e*a45nd@se   c?UR )"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="re_password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Confirm password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="nic#e*a45nd@se   c?UR )"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button>Sign up</Button>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter>
+            <p className="text-light text-sm text-gray-400">
+              Already have an account?{" "}
+              <Link to="/sign-in" className="underline text-gray-600">
+                Sign in!
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
   )
 }
