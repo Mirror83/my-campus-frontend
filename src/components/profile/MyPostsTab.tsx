28abf02db3aeee07c017d20cab99bf639eeb814d
Mirror83 <glenochieng045@gmail.com>
2024-03-06 08:03:48 +0300

    Laid down a few bones for the profile page

---
+++ src/components/profile/MyPostsTab.tsx
@@ -0,0 +1,34 @@
+import { Blog } from "@/interfaces/blog"
+import BlogCard from "../home/BlogCard"
+import { Link } from "react-router-dom"
+import { Button } from "../ui/button"
+import { Plus } from "lucide-react"
+
+type Props = {
+  blogs: Blog[]
+}
+
+export function MyPosts({ blogs }: Props) {
+  const blogElements = blogs.map((blog, i) => <BlogCard blog={blog} key={i} />)
+  return (
+    <div className="flex items-center justify-center min-h-[400px]">
+      {blogs.length == 0 ? (
+        <>
+          <div className="text-center">
+            <p className="text-bold text-3xl mb-4"> No blogs currently.</p>
+            <p className="mb-4">
+              But there's no better time than the present 😊.{" "}
+            </p>
+
+            <Button>
+              <Plus className="me-2" />
+              <Link to="/write-blog">Create one</Link>
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div>{blogElements}</div>
+      )}
+    </div>
+  )
+}
--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,7 +1,137 @@
+import { useAppSelector } from "@/app/hook"
+import { user } from "@/app/services/authSlice"
+import { AuthorAvatar } from "@/components/blog-post/AuthorAvatar"
+import { MyPosts } from "@/components/profile/MyPostsTab"
+import { Button } from "@/components/ui/button"
+import { Blog } from "@/interfaces/blog"
+import { cn } from "@/lib/utils"
+import { ArrowLeftIcon } from "@radix-ui/react-icons"
+import { ArrowBigLeft, Loader2 } from "lucide-react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+enum ProfilePageTabs {
+  MyPosts = "My Blog Posts",
+  Actions = "Actions",
+  ProfileInfo = "Profile Info",
+}
+
 export function Profile() {
+  const { user, isLoading } = useAppSelector(state => state.auth)
+  const [activeTab, setActiveTab] = useState(ProfilePageTabs.MyPosts)
+
+  function changeTab(tab: ProfilePageTabs) {
+    setActiveTab(tab)
+  }
+
   return (
-    <>
-      <h1>Profile</h1>
-    </>
+    <div className="p-4">
+      <div className="w-full p-4">
+        <Link to="/">
+          <Button variant={"link"} className="flex gap-2 items-center">
+            <ArrowLeftIcon />
+            <span className="text-sm">Back Home</span>
+          </Button>
+        </Link>
+      </div>
+      <div className="px-8">
+        {user ? (
+          <div className="flex gap-16 items-center">
+            <div className="flex gap-4 items-center">
+              <AuthorAvatar
+                src=""
+                authorName={`${user?.first_name} ${user?.last_name}`}
+                className="h-[100px] w-[100px]"
+              />
+              <div>
+                <h2 className="text-3xl">{user?.username}</h2>
+                <small className="light text-sm text-gray-500">
+                  {`@${user?.username}`}
+                </small>
+              </div>
+            </div>
+            <div>
+              <div className="text-lg font-bold text-center">
+                {user?.followers}
+              </div>
+              <div>Followers</div>
+            </div>
+            <div>
+              <div className="text-lg font-bold text-center">
+                {user?.following}
+              </div>
+              <div>Following</div>
+            </div>
+          </div>
+        ) : (
+          <Loader2 className="w-4 h-4 animate-spin" />
+        )}
+        <div className="mt-8">
+          <div className="flex gap-4 overflow-x-auto p-2 font-light">
+            <TabActivator
+              tab={ProfilePageTabs.MyPosts}
+              currentTab={activeTab}
+              onChangeTab={changeTab}
+            />
+            <TabActivator
+              tab={ProfilePageTabs.ProfileInfo}
+              currentTab={activeTab}
+              onChangeTab={changeTab}
+            />
+            <TabActivator
+              tab={ProfilePageTabs.Actions}
+              currentTab={activeTab}
+              onChangeTab={changeTab}
+            />
+          </div>
+          <div className="my-4 bg-slate-50 min-h-[400px]">
+            <ActiveTabContent currentTab={activeTab} blogs={[]} user={user} />
+          </div>
+        </div>
+      </div>
+    </div>
   )
+}
+
+type TabActivatorProps = {
+  tab: ProfilePageTabs
+  currentTab: ProfilePageTabs
+  onChangeTab: (tab: ProfilePageTabs) => void
+}
+
+function TabActivator({ tab, currentTab, onChangeTab }: TabActivatorProps) {
+  return (
+    <Button
+      variant={"ghost"}
+      className={cn(currentTab == tab && "font-bold border-b-4 border-black")}
+      onClick={() => onChangeTab(tab)}
+    >
+      {tab.valueOf()}
+    </Button>
+  )
+}
+
+type CurrentTabProps = {
+  currentTab: ProfilePageTabs
+  blogs: Blog[]
+  user: user | undefined | null
+}
+
+function ActiveTabContent({ currentTab, blogs, user }: CurrentTabProps) {
+  if (currentTab === ProfilePageTabs.MyPosts) {
+    return <MyPosts blogs={blogs} />
+  }
+  if (currentTab === ProfilePageTabs.Actions) {
+    return <div>Actions</div>
+  } else {
+    return (
+      <div className="flex items-center justify-center h-full">
+        {user && (
+          <>
+            <p className="text-3xl italic">Coming soon</p>
+          </>
+        )}
+      </div>
+    )
+  }
 }
