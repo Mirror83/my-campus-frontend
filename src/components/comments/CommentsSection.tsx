68f3237ebf1d73f02b3e5a3837a3464bdf8d0818
Mirror83 <glenochieng045@gmail.com>
2024-03-13 20:20:11 +0300

    Implemented form for addition of comments to json-server mock backend

--- src/components/comments/CommentsSection.tsx
+++ src/components/comments/CommentsSection.tsx
@@ -1,37 +1,54 @@
+import { BlogComment } from "@/interfaces/comment"
 import { AuthorAvatar } from "../blog-post/AuthorAvatar"
 import { Button } from "../ui/button"
+import { Form, useSubmit } from "react-router-dom"
+import { useState } from "react"
 
 interface CommentSectionProps {
-  comments: CommentData[]
+  comments: BlogComment[]
 }
 
 interface CommentProps {
-  comment: CommentData
-}
-
-export interface CommentData {
-  userImage: string
-  userName: string
-  content: string
+  comment: BlogComment
 }
 
 export function CommentsSection({ comments }: CommentSectionProps) {
+  const [commentContent, setCommentContent] = useState("")
   const commentElements = comments.map(comment => (
-    // The key here should be an id field
-    <Comment comment={comment} key={comment.userName} />
+    <Comment comment={comment} key={comment.unique_id} />
   ))
+
+  const submit = useSubmit()
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const data = {
+      content: commentContent,
+    }
+    submit(JSON.stringify(data), {
+      method: "post",
+      encType: "application/json",
+    })
+  }
+
   return (
     <div className="bg-slate-200 mtx-8 min-h-[200px] p-4 rounded-sm">
       <h2 className="text-3xl my-4">Comments</h2>
       <div className="">
-        <form>
+        <Form method="post" onSubmit={handleSubmit}>
           <input
             name="comment"
             placeholder="Share your thoughts here..."
             className="p-2 w-4/6 me-4 rounded-sm"
+            value={commentContent}
+            onChange={event => {
+              console.log("input: " + event.target.value)
+              setCommentContent(event.target.value)
+            }}
+            required={true}
           />
           <Button>Send</Button>
-        </form>
+        </Form>
       </div>
       <div className="mt-4">
         <p className="text-sm text-gray-600">
@@ -47,8 +64,8 @@ export function CommentsSection({ comments }: CommentSectionProps) {
 function CommentorDetailsRow({ comment }: CommentProps) {
   return (
     <div className="flex items-center gap-2">
-      <AuthorAvatar authorName={comment.userName} src={comment.userImage} />
-      <h3 className="text-bold text-xl">{comment.userName}</h3>
+      <AuthorAvatar authorName={comment.commentor} src={""} />
+      <h3 className="text-bold text-xl">{comment.commentor}</h3>
     </div>
   )
 }
@@ -57,7 +74,7 @@ function Comment({ comment }: CommentProps) {
   return (
     <div className="p-4">
       <CommentorDetailsRow comment={comment} />
-      <div className="p-2">{comment.content}</div>
+      <div className="p-2">{comment.comment}</div>
     </div>
   )
 }
