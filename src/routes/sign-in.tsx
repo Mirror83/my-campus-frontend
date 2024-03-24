0f8e969a153c5aa3fb8ba81e2c1bc3be8a8847f9
Mirror83 <glenochieng045@gmail.com>
2024-03-24 09:45:59 +0300

    Updated sign-in and sign-up to use layout in design

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,14 +1,5 @@
-import GraduationCapIcon from "@/components/home/GraduationCapIcon"
 import { Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import {
   Form,
   FormControl,
@@ -19,25 +10,29 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useNavigate } from "react-router"
-import { Link } from "react-router-dom"
 import { useSignIn } from "@/hooks"
 import { toast } from "sonner"
 import { useAppSelector } from "@/app/hook"
 import { useEffect } from "react"
-
-const formSchema = z.object({
-  email: z.string().email().trim(),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters long",
-  }),
-})
+import {
+  SignInFormSchema,
+  SignInFormValidator,
+} from "@/lib/validators/form-schema"
+import { AuthFormLayout } from "@/components/forms/AuthFormLayout"
 
 export function SignIn() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  return (
+    <AuthFormLayout isSignUp={false} imageUrl="/images/signin-image.png">
+      <SignInForm />
+    </AuthFormLayout>
+  )
+}
+
+function SignInForm() {
+  const form = useForm<SignInFormValidator>({
+    resolver: zodResolver(SignInFormSchema),
     defaultValues: {
       email: "",
       password: "",
@@ -49,89 +44,62 @@ export function SignIn() {
   const { signInUser, isLoading } = useSignIn()
   const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated)
 
-  function onSubmit(formValues: z.infer<typeof formSchema>) {
+  function onSubmit(formValues: SignInFormValidator) {
     console.log(formValues)
     signInUser(formValues)
   }
 
   useEffect(() => {
-    if (isAuthenticated === true) {
+    if (isAuthenticated) {
       navigate("/")
       toast.success("You have successfully signed in.")
     }
   }, [isAuthenticated, navigate])
 
   return (
-    <div className={"grid h-[100vh] grid-cols-[2fr_3fr]"}>
-      <div className="bg-black"></div>
-      <div className="flex flex-col items-center justify-center">
-        <Card className="w-[400px]">
-          <CardHeader>
-            <CardTitle>
-              <div className="flex gap-1 items-center text-2xl">
-                <GraduationCapIcon />
-                <h1>My Campus</h1>
-              </div>
-            </CardTitle>
-            <CardDescription className="text-xl">Welcome back</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <div className="flex flex-col gap-2 mb-4">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                          <Input placeholder="mycampus" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-[500px]">
+        <div className="flex flex-col gap-2 mb-4 px-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="mycampus" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="nic#e*a45nd@se   c?UR )"
+                    {...field}
                   />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="nic#e*a45nd@se   c?UR )"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                {isLoading ? (
-                  <Button disabled>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    Please wait
-                  </Button>
-                ) : (
-                  <Button>Sign in</Button>
-                )}
-              </form>
-            </Form>
-          </CardContent>
-          <CardFooter>
-            <p className="text-light text-sm text-gray-400">
-              Don't have an account?{" "}
-              <Link to="/sign-up" className="underline text-gray-600">
-                Sign up
-              </Link>{" "}
-              for free.
-            </p>
-          </CardFooter>
-        </Card>
-      </div>
-    </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        {isLoading ? (
+          <Button disabled>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            Please wait
+          </Button>
+        ) : (
+          <Button>Sign in</Button>
+        )}
+      </form>
+    </Form>
   )
 }
--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,14 +1,5 @@
 import { useAppSelector } from "@/app/hook"
-import GraduationCapIcon from "@/components/home/GraduationCapIcon"
 import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import {
   Form,
   FormControl,
@@ -20,56 +11,42 @@ import {
 import { Input } from "@/components/ui/input"
 import { useSignUp } from "@/hooks"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form"
-import { Link, useNavigate } from "react-router-dom"
-import { z } from "zod"
-
-// The naming convention used by the server is snake case
-// I know, it brings a lot of confusion 😂.
-const formSchema = z.object({
-  email: z.string().email(),
-  // Find a way to make the names into title case (or let the server do it)
-  first_name: z
-    .string()
-    .min(2, {
-      message: "First name should be at least 2 characters long",
-    })
-    .trim(),
-  last_name: z
-    .string()
-    .min(2, {
-      message: "Last name should be at least 2 characters long",
-    })
-    .trim(),
-  username: z
-    .string()
-    .min(2, {
-      message: "A username must be at least 2 characters long",
-    })
-    .trim(),
-  password: z.string().min(8, {
-    message: "Your password must be at least 8 characters long",
-  }),
-
-  re_password: z.string().min(8, {
-    message: "Your password must be at least 8 characters long",
-  }),
-})
-
-export type FormUser = z.infer<typeof formSchema>
+import { useNavigate } from "react-router-dom"
+import {
+  SignUpFormSchema,
+  SignUpFormValidator,
+} from "@/lib/validators/form-schema"
+import { AuthFormLayout } from "@/components/forms/AuthFormLayout"
 
 // TODO: Store the layout that this shares with the sign-in page
 //       and use that as a root layout for both to reduce duplication
 export function SignUp() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  return (
+    <AuthFormLayout isSignUp={true}>
+      <SignUpForm />
+    </AuthFormLayout>
+  )
+}
+
+function SignUpForm() {
+  const form = useForm<SignUpFormValidator>({
+    resolver: zodResolver(SignUpFormSchema),
   })
   const { signUpUser } = useSignUp()
+
   const user = useAppSelector(state => state.auth.user)
+
   const navigate = useNavigate()
 
-  function onSubmit(formValues: z.infer<typeof formSchema>) {
+  const [showPassword, setShowPassword] = useState(false)
+
+  function togglePasswordVisibility() {
+    setShowPassword(prevState => !prevState)
+  }
+
+  function onSubmit(formValues: SignUpFormValidator) {
     console.log(formValues)
     signUpUser(formValues)
   }
@@ -79,132 +56,101 @@ export function SignUp() {
   }, [user, navigate])
 
   return (
-    <div className={"grid h-[100vh] grid-cols-[2fr_3fr]"}>
-      <div className="bg-black"></div>
-      <div className="flex flex-col items-center justify-center">
-        <Card className="w-[500px] h-[100vh] overflow-y-auto my-4">
-          <CardHeader>
-            <CardTitle>
-              <div className="flex gap-1 items-center text-2xl">
-                <GraduationCapIcon />
-                <h1>My Campus</h1>
-              </div>
-            </CardTitle>
-            <CardDescription className="text-xl">
-              Create an account
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <div className="flex flex-col gap-2 mb-4">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder="kimani.mwendwa@students.jkuat.ac.ke"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-[500px]">
+        <div className="flex flex-col gap-2 mb-4 px-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="kimani.mwendwa@students.jkuat.ac.ke"
+                    {...field}
                   />
-                  <FormField
-                    control={form.control}
-                    name="first_name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>First name</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Kimani" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="first_name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Kimani" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="last_name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Mwendwa" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="awesomeuser" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="nic#e*a45nd@se   c?UR )"
+                    {...field}
                   />
-                  <FormField
-                    control={form.control}
-                    name="last_name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Last name</FormLabel>
-                        <FormControl>
-                          <Input
-                            placeholder="kimani.mwendwa@students.jkuat.ac.ke"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="re_password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="nic#e*a45nd@se   c?UR )"
+                    {...field}
                   />
-                  <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                          <Input placeholder="mycampus" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="nic#e*a45nd@se   c?UR )"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="re_password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Confirm password</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder="nic#e*a45nd@se   c?UR )"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <Button>Sign up</Button>
-              </form>
-            </Form>
-          </CardContent>
-          <CardFooter>
-            <p className="text-light text-sm text-gray-400">
-              Already have an account?{" "}
-              <Link to="/sign-in" className="underline text-gray-600">
-                Sign in!
-              </Link>
-            </p>
-          </CardFooter>
-        </Card>
-      </div>
-    </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button>Sign up</Button>
+        </div>
+      </form>
+    </Form>
   )
 }
