7d1e3e37fe5b11cf4210e703a3d9ae2f9e56501a
Mirror83 <glenochieng045@gmail.com>
2024-03-25 17:09:48 +0300

    Added mock-comments.ts

---
+++ src/mock-content/mock-comments.ts
@@ -0,0 +1,12 @@
+import { BlogComment } from "@/interfaces/comment"
+
+export const mockComments: BlogComment[] = [
+  {
+    unique_id: "1",
+    comment: "Nicely put. I love how concise that was",
+    blog_slug: "kin",
+    commentor: "Ben Jorndan",
+    created_at: "25-10-2023",
+    updated_at: "25-10-2023",
+  },
+]
