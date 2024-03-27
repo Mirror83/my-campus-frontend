829f3fe9ba6f3b3a7a8af6e0c53cc8d968d0c4e4
Mirror83 <glenochieng045@gmail.com>
2024-03-27 14:22:48 +0300

    Updataed interface to mirror more what was presented by the backend

--- src/interfaces/blog.ts
+++ src/interfaces/blog.ts
@@ -1,14 +1,13 @@
-// TODO: Update the content field, for now it just holds a string summary so that I can render it 
-//       the BlogCard elements
+import { OutputData } from "@editorjs/editorjs"
+
 export interface Blog {
     title: string
     topics: string[]
-    content: string 
+    content: OutputData 
     imageUrl: string
-    /**The authorSlug is a unique id for each blog post and it associates each post to a user */
-    /**Will just append slug to url for reading files */
-    // authorSlug: string
-
-    // Replace author slug with author name for display purposes for now
-    authorName: string
+    slug: string
+    author: string
+    likes: number
+    followers: number
+    following: number
 }
