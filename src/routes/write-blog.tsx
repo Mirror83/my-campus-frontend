d00739d072bcc865d789fa8b0e6735580777e48e
Mirror83 <glenochieng045@gmail.com>
2024-03-27 11:09:34 +0300

    Revised submission and elements involved in it.

--- src/routes/write-blog.tsx
+++ src/routes/write-blog.tsx
@@ -1,8 +1,7 @@
 //index.tsx
-import type { OutputData } from "@editorjs/editorjs"
-import React, { useEffect, useState } from "react"
+import type React from "react"
+import { useEffect, useRef, useState } from "react"
 import { NavBar } from "@/components/home/NavBar"
-import { blogPlaceholder } from "@/mock-content/mock-text"
 import { Button } from "@/components/ui/button"
 import Editor from "@/components/editor/Editor"
 import {
@@ -28,7 +27,7 @@ import {
 
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
-import { Topic } from "@/interfaces/topics"
+import type { Topic } from "@/interfaces/topics"
 import axios from "@/lib/axios"
 import { Badge } from "@/components/ui/badge"
 import { Cross1Icon } from "@radix-ui/react-icons"
@@ -38,21 +37,43 @@ import { Loader2Icon } from "lucide-react"
 import { useNavigate } from "react-router"
 import LoadingScreen from "@/components/loading-screen/LoadingScreen"
 import { mockTopics } from "@/mock-content/mock-topics"
+import { OutputData } from "@editorjs/editorjs"
+
+const topicLimit = 3
 
 export const WriteBlog = () => {
   const { user, isLoading } = useAppSelector(state => state.auth)
-  // Stores the data used by EditorJS
-  const [data, setData] = useState<OutputData>(blogPlaceholder)
+
+  const [editorData, setEditorData] = useState<OutputData>({ blocks: [] })
   const [title, setTitle] = useState("")
+
   const [topics, setTopics] = useState<Topic[]>([])
   const [selectedTopics, setSelectedTopics] = useState<Set<Topic>>(new Set())
-  const selectedTopicsBagdes: React.ReactNode[] = []
+  const selectedTopicsBadges: React.ReactNode[] = []
+
   const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  const titleRef = useRef<HTMLTextAreaElement>(null)
+
   const navigate = useNavigate()
-  // const topicLimit = 4
+
+  useEffect(() => {
+    if (titleRef.current?.textContent?.length === 0) titleRef.current?.focus()
+  }, [])
+
+  // useEffect(() => {
+  //   if (!(user && isAuthenticated && isVerified)) {
+  //     navigate("/sign-in", { replace: true })
+  //     toast.error("Authorization Error", {
+  //       description: "You need to sign in to create a blog post.",
+  //     })
+  //   }
+  // }, [])
 
   selectedTopics.forEach(topic =>
-    selectedTopicsBagdes.push(
+    selectedTopicsBadges.push(
       <Badge
         variant={"outline"}
         key={topic.id}
@@ -78,21 +99,14 @@ export const WriteBlog = () => {
     getTopics()
   }, [])
 
-  useEffect(() => {
-    trackTitle()
-  }, [data])
-
-  function trackTitle() {
-    const firstHeading = data.blocks.find(block => block.type === "header")
-    if (firstHeading === undefined) {
-      setTitle("")
-    } else {
-      setTitle(firstHeading.data.text)
-    }
-  }
-
   function selectTopic(topic: Topic) {
-    setSelectedTopics(prev => new Set(prev.add(topic)))
+    if (selectedTopics.size <= topicLimit - 1) {
+      setSelectedTopics(prev => new Set(prev.add(topic)))
+    } else {
+      toast.warning("Too many topics selected", {
+        description: `You can associate your creation with a maximum of ${topicLimit} topics`,
+      })
+    }
   }
 
   function removeTopic(topic: Topic) {
@@ -105,12 +119,24 @@ export const WriteBlog = () => {
   }
 
   async function submit() {
-    if (title === "") {
-      toast("You need a to place a heading in your writing.")
+    if (!title) {
+      toast.warning("Title is required", {
+        description: "Please provide a title for your blog post.",
+      })
+      setIsDialogOpen(false)
+      return
+    }
+
+    if (editorData.blocks.length === 0) {
+      toast.warning("Content is required", {
+        description: "Please write something in the editor.",
+      })
+      setIsDialogOpen(false)
       return
     }
 
     const topicsList: { topic_name: string }[] = []
+
     selectedTopics.forEach(topic =>
       topicsList.push({
         topic_name: topic.topic_name,
@@ -120,9 +146,8 @@ export const WriteBlog = () => {
     const blogCreationData = {
       title,
       author_type: "user",
-      // Obtain user object
       author: user?.username,
-      content: data,
+      content: editorData,
       topics: topicsList,
     }
 
@@ -138,16 +163,15 @@ export const WriteBlog = () => {
         },
       )
       console.log(response)
-      if (response.status == 201) {
-        toast("Successfully created a blog post!", {
-          action: {
-            label: "View in profile",
-            onClick: () => navigate("/profile"),
-          },
-        })
+      if (response.status === 201) {
+        navigate("/profile")
+        toast.success("Successfully created the blog post!")
       }
     } catch (err) {
-      console.error(err)
+      console.log(err)
+      toast.error("Unable to create the blog post!", {
+        description: "Please try again later.",
+      })
     } finally {
       setIsSubmitting(false)
     }
@@ -158,13 +182,27 @@ export const WriteBlog = () => {
   }
 
   return (
-    <Dialog>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <div>
         {/**Replace this with custom navbar for the read page */}
         <NavBar />
         <div className="grid grid-cols-12">
           <div className="p-4 col-span-8">
-            <Editor />
+            <textarea
+              name="title"
+              placeholder="Title"
+              className="text-4xl font-bold p-4 mx-2 w-full h-[70px]"
+              maxLength={100}
+              ref={titleRef}
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+            <Editor
+              title={title}
+              setTitle={(title: string) => setTitle(title)}
+              editorData={editorData}
+              setEditorData={(data: OutputData) => setEditorData(data)}
+            />
           </div>
           <div className="p-4">
             <div className="flex gap-4">
@@ -185,37 +223,27 @@ export const WriteBlog = () => {
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Title
-            </Label>
-            <Input
-              id="name"
-              disabled={true}
-              value={title}
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="featured-image" className="text-right">
-              Featured Image
+              Thumbnail Image
             </Label>
             <Input id="featured-image" type="file" className="col-span-3" />
           </div>
           <div>
             <h4 className="text-lx py-2 font-medium">Topics</h4>
-            <div className="flex gap-2 py-2">{selectedTopicsBagdes}</div>
-            <Command>
+            <div className="flex gap-2 py-2 flex-wrap">
+              {selectedTopicsBadges}
+            </div>
+            <Command className="h-[150px] overflow-hidden">
               <CommandInput placeholder="Search for a topic to associate with your work" />
               <CommandList>
                 <CommandEmpty>No results found.</CommandEmpty>
                 {/**Poised to include suggestions here */}
                 <CommandSeparator />
-                <CommandGroup heading="Topics">
+                <CommandGroup>
                   {topics.map(topic => (
                     <CommandItem
                       key={topic.id}
                       onSelect={() => selectTopic(topic)}
-                      onClick={() => selectTopic(topic)}
                       className={selectedTopics.has(topic) ? "font-bold" : ""}
                     >
                       {topic.topic_name}
@@ -227,7 +255,11 @@ export const WriteBlog = () => {
           </div>
         </div>
         <DialogFooter>
-          <Button onClick={submit} disabled={isSubmitting}>
+          <Button
+            onClick={submit}
+            form="write-blog-form"
+            disabled={isSubmitting}
+          >
             {isSubmitting && (
               <Loader2Icon className="w-4 h-4 me-2 animate-spin" />
             )}
