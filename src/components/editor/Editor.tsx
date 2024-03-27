81b50a3fc6e3573e854480ecbca1e2309c9c1d6c
Mirror83 <glenochieng045@gmail.com>
2024-03-27 11:08:05 +0300

    Moved textarea, removed submission logic, reverted editor to use state passed as props, memoized Editor.

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,26 +1,35 @@
-import type EditorJS from "@editorjs/editorjs"
-import type { OutputData } from "@editorjs/editorjs"
-import React, { useCallback, useEffect, useRef, useState } from "react"
+// @ts-ignore
+import Header from "@editorjs/header"
+// @ts-ignore
+import Image from "@editorjs/image"
+// @ts-ignore
+import List from "@editorjs/list"
+
+import EditorJS, { OutputData } from "@editorjs/editorjs"
+
+import { memo, useEffect, useRef } from "react"
 import { BASE_URL } from "@/constants"
-import { usePlugins } from "@/lib/editor/usePlugins"
 
-function Editor() {
-  const ref = useRef<EditorJS>()
+interface EditorProps {
+  title: string
+  setTitle: (title: string) => void
+  editorData: OutputData
+  setEditorData: (data: OutputData) => void
+}
 
-  // Stream in the editor as it is large and will take a while to load
-  const initializeEditor = useCallback(async () => {
-    const { EditorJS, Image, List, Header } = await usePlugins()
-    // If editor is not initialized
-    if (!ref.current) {
-      const editor = new EditorJS({
+function Editor({ editorData, setEditorData }: EditorProps) {
+  const editorRef = useRef<EditorJS>()
+
+  useEffect(() => {
+    if (!editorRef.current) {
+      editorRef.current = new EditorJS({
         holder: "editor-container",
-        placeholder: "Type your content here...",
-        onReady: () => {
-          // So that we do not re-initialize the editor
-          ref.current = editor
-          console.log("Editor is ready")
+        placeholder: "Write something awesome!",
+        data: editorData,
+        async onChange(api, _event) {
+          const data = await api.saver.save()
+          setEditorData(data)
         },
-        inlineToolbar: true,
         tools: {
           header: Header,
           list: List,
@@ -37,51 +46,20 @@ function Editor() {
           },
         },
       })
-      ref.current = editor
-    }
-  }, [])
-
-  useEffect(() => {
-    async function createEditor() {
-      await initializeEditor()
-    }
-
-    console.log(ref)
-
-    if (!ref.current) {
-      createEditor()
     }
 
     return () => {
-      if (ref.current && ref.current.destroy) {
-        ref.current.destroy()
-        ref.current = undefined
+      if (editorRef.current && editorRef.current.destroy) {
+        editorRef.current.destroy()
       }
     }
-  }, [ref, initializeEditor])
-
-  async function submit(event: React.FormEvent) {
-    event.preventDefault()
-    const blocks = await ref.current?.save()
-    if (blocks) {
-      console.log(blocks)
-    } else {
-      console.log("Unable to get blocks.")
-    }
-  }
+  }, [])
 
   return (
     <div className="prose min-h-[500px] max-w-full">
-      <form id="write-blog-form" onSubmit={submit}>
-        <textarea
-          placeholder="Title"
-          className="text-4xl p-4 mx-2 w-full appearance-none active:border-0"
-          maxLength={100}
-        ></textarea>
-        <div id="editor-container" />
-      </form>
+      <div id="editor-container" className="pt-2 transition-colors" />
     </div>
   )
 }
 
-export default Editor
+export default memo(Editor)
