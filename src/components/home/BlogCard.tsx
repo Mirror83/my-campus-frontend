bcaa8b786b56a5aa7366b5ffb8c798eca86b5cb9
Mirror83 <glenochieng045@gmail.com>
2024-03-28 17:09:59 +0300

    Added excerpt

--- src/components/home/BlogCard.tsx
+++ src/components/home/BlogCard.tsx
@@ -11,6 +11,8 @@ interface BlogCardProps {
 // TODO: Get author for corresponding post
 // TODO: Include topics in the design
 function BlogCard({ blog }: BlogCardProps) {
+  const excerpt: string = blog.excerpt.slice(0, 100) + "..."
+
   return (
     <div className="grid p-2 outline-1 outline rounded-sm">
       {/**Image */}
@@ -27,6 +29,7 @@ function BlogCard({ blog }: BlogCardProps) {
 
         <h3 className="text-2xl">{blog.title}</h3>
         {/**To add back the blog post excerpt. */}
+        <p>{excerpt}</p>
         <p className="font-light">
           <Link to={`/read-blog/${blog.slug}`} className="underline">
             Read more.
--- src/interfaces/blog.ts
+++ src/interfaces/blog.ts
@@ -3,7 +3,8 @@ import { OutputData } from "@editorjs/editorjs"
 export interface Blog {
     title: string
     topics: string[]
-    content: OutputData 
+    content: OutputData
+    excerpt: string
     imageUrl: string
     slug: string
     author: string
