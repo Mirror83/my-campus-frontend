e8c69979fbed9f56b550992c0b454c575242e531
Mirror83 <glenochieng045@gmail.com>
2024-04-03 01:00:34 +0300

    Added link to profile based on author type. Also added fake liking.

--- src/components/blog-post/BlogPostMetaData.tsx
+++ src/components/blog-post/BlogPostMetaData.tsx
@@ -1,8 +1,12 @@
-import { ChatBubbleIcon, PlusIcon } from "@radix-ui/react-icons"
+import { ChatBubbleIcon } from "@radix-ui/react-icons"
 import { AuthorAvatar } from "./AuthorAvatar"
+import { Link, useFetcher } from "react-router-dom"
+import { ThumbsUp } from "lucide-react"
+import { useState } from "react"
 
 interface BlogPostMetaDataProps {
   authorName: string
+  authorType?: string
   dateCreated: string
   totalComments: number
   totalLikes: number
@@ -11,29 +15,38 @@ interface BlogPostMetaDataProps {
 
 export function BlogPostMetaData({
   authorName,
+  authorType = "user",
   dateCreated,
   totalComments,
   totalLikes,
   className,
 }: BlogPostMetaDataProps) {
+  const fetcher = useFetcher()
+  const [isLiked, setIsLiked] = useState(false)
+
   return (
     <div className={className}>
       <div className="flex gap-4 py-2 items-center text-sm">
-        <AuthorAvatar
-          src="https://github.com/shadcn.png"
-          authorName={authorName}
-        />
-        <strong>{authorName}</strong>
+        <Link
+          to={`/profile/${authorType}/${authorName}`}
+          className="flex gap-4 items-center"
+        >
+          <AuthorAvatar src="" authorName={authorName} />
+          <strong>{authorName}</strong>
+        </Link>
+
         <em>{dateCreated}</em>
       </div>
       <div className="flex gap-4 py-1 items-center">
-        <div className="flex gap-1 items-center">
-          <ChatBubbleIcon />
+        <div className="flex gap-2 items-center">
+          <ChatBubbleIcon className="h-6 w-6" />
           <em>{totalComments}</em>
         </div>
-        <div className="flex gap-1 items-center">
-          <PlusIcon />
-          <em>{totalLikes}</em>
+        <div className="flex gap-2 items-center">
+          <button onClick={() => setIsLiked(!isLiked)}>
+            <ThumbsUp className="h-6 w-6" fill={isLiked ? "#242748" : "none"} />
+          </button>
+          <em>{isLiked ? totalLikes + 1 : totalLikes}</em>
         </div>
       </div>
     </div>
